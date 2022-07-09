047025a59f7b0f24c690f5e7a41910f6cd41072b
Gabriel Roberto <gabrielrobertoal@gmail.com>
2022-07-09 15:24:30 -0300

    recreation nihongo quiz

--- nihongo/nihongo-quiz/src/views/pages/Katakana/Katakana.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Katakana = () => {
-  return (
-    <div>Katakana</div>
-  )
-}
-
-export default Katakana
--- nihongo/nihongo-quiz/src/views/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Hiragana } from "./Hiragana/Hiragana";
-export { default as Katakana } from "./Katakana/Katakana";
-export { default as About } from "./About/About";
-export { default as NotFound } from "./NotFound/NotFound";
