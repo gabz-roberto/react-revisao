dc77cdd2c38a856e0a620ebc70cc9dbdceccf4da
Gabriel Roberto <gabrielrobertoal@gmail.com>
2022-07-25 11:07:16 -0300

    webpack

--- ToDo/server/src/config/server.js
+++ ToDo/server/src/config/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const allowCors = require('./cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(allowCors);
 
 const PORT = 3003;
 
